72074b3d7814c10b2e19317a5505ddb00d4b118e
JY Choi <jykchoi@gmail.com>
2022-03-28 21:12:17 +0900

    #2 done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
 
 const INITAIL_COLOR = "#2c2c2c"
 const CANVAS_SIZE = 700
@@ -67,7 +69,15 @@ function handleCanvasClick(){
 }
 
 function handleCM(event){
-    event.preventDefault
+    event.preventDefault()
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click();
 }
 
 if(canvas) {
@@ -88,4 +98,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick); 
 }
